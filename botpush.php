3663fa1889880d4be3f0de3591ba82648cd9e967
Puritz <pop-purit@hotmail.com>
2018-05-18 16:44:31 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -85,7 +85,7 @@ if(!is_null($events)){
     }
 }
 // ส่วนของคำสั่งจัดเตียมรูปแบบข้อความสำหรับส่ง
-$textMessageBuilder = new TextMessageBuilder(json_encode($events));
+$textMessageBuilder = new TextMessageBuilder($textReplyMessage);
  
 //l ส่วนของคำสั่งตอบกลับข้อความ
 $response = $bot->replyMessage($replyToken,$textMessageBuilder);
