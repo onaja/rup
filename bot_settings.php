c0a24afc2a8bd72d72000a2dfbd146c5d59825e0
Puritz <pop-purit@hotmail.com>
2018-05-18 16:36:10 +0700

    Create bot_settings.php

---
+++ bot_settings.php
@@ -0,0 +1,6 @@
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('LINE_MESSAGE_CHANNEL_ID','1581202421');
+define('LINE_MESSAGE_CHANNEL_SECRET','74f635ba1154370dd18c994619068ac8');
+define('LINE_MESSAGE_ACCESS_TOKEN','nQiY++j/wbNA8u6zlNzz/RtafsC2i40ERqGEgtvxNDXwTTt9Z62ZdAxwwceZk3WqPWBquHunHE0HZtRL8Ezq9xf7cxTdeI/fKSKy9uNqwBIAk+GCO+4DYuhGzchTA1jWATfAYoSH7oEJNRTobVOdIAdB04t89/1O/w1cDnyilFU=');
+?>
