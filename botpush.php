75d2e2001e31bed4c37f741c5e1cdff9499907a4
Puritz <pop-purit@hotmail.com>
2018-10-20 22:54:25 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -76,9 +76,8 @@
     $arrayPostData['messages'][0]['text'] = 'คุณสามารถสอนให้ฉลาดได้เพียงพิมพ์: สอนบอท[คำถาม|คำตอบ]';
     $arrayPostData['messages'][1]['type'] = "text";
     $arrayPostData['messages'][1]['text'] = $id;
-    $arrayPostData['messages'][2]['type'] = "template";
-    $arrayPostData['messages'][2]['altText'] = 'Are you sure';
     $arrayPostData['messages'][2]['type'] = "confirm";
+    $arrayPostData['messages'][2]['text'] = 'test';
     replyMsg($arrayHeader,$arrayPostData);
     
   }
